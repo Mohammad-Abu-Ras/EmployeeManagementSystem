d3c553b6cee5437e1ae8d372752abc7f9b6ac0fa
Mohammad Ras <mohammed.g.aburass@gmail.com>
2025-11-26 12:07:36 +0300

    Add test to the main

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Welcome to Employee Management System V1!");
-
+        System.out.println("this is test!");
     }
 }
