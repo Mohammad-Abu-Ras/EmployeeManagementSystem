66c3019ef0151340c6f46c381f97590b17876f5f
Mohammad Ras <mohammed.g.aburass@gmail.com>
2025-11-26 12:15:23 +0300

    remover everything to start again

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,5 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Employee Management System V1!");
-        System.out.println("this is test!");
     }
 }
