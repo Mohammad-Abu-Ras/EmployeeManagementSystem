21d75ab5bb3fd97a3a1ad39d01b05a67466ae57f
Mohammad Ras <mohammed.g.aburass@gmail.com>
2025-11-29 22:12:35 +0300

    create dynamic statement to reduce the repetition in the code

--- src/EmployeeDAO.java
+++ src/EmployeeDAO.java
@@ -1,2 +1,14 @@
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.sql.PreparedStatement;
+
 public class EmployeeDAO {
+
+    // here creating prepared statement for given sql:
+
+    private PreparedStatement creatStatement (String sql) throws SQLException{
+        return DBConnection.getConnection().prepareStatement(sql);
+    }
+
 }
+//
