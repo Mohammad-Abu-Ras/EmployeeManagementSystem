398593b1a6a56cadb5e9aaf2fff523811c8de5bd
Mohammad Ras <mohammed.g.aburass@gmail.com>
2025-11-29 22:35:53 +0300

    adding applyParameter to dealing with parameters

--- src/EmployeeDAO.java
+++ src/EmployeeDAO.java
@@ -6,9 +6,24 @@ public class EmployeeDAO {
 
     // here creating prepared statement for given sql :
 
-    private PreparedStatement creatStatement (String sql) throws SQLException{
+    private PreparedStatement creatStatement(String sql) throws SQLException {
         return DBConnection.getConnection().prepareStatement(sql);
     }
+
+
+    // we need this method to  avoid repeating stmt.setXxx(...) in every CRUD method ,and makes DAO dynamic:
+    // any sql with any number of parameters works also keeps code clean dont repeat yourself and prevents boilerplate.
+
+    private void applyParameter(PreparedStatement st, Object[] parameters) throws SQLException {
+        if (parameters != null) {
+            return;
+        }
+        int size = parameters.length;
+        for (int i = 0; i < size; i++) {
+            st.setObject(i + 1, parameters[i]);
+        }
+    }
+
 
 }
 //
