a6790c8740d8fcb26c5b8afba9518065f4e4069c
Mohammad Ras <mohammed.g.aburass@gmail.com>
2025-11-30 00:24:25 +0300

    the class is ready!

--- src/InputOutputAndValidation.java
+++ src/InputOutputAndValidation.java
@@ -128,11 +128,6 @@ public class InputOutputAndValidation {
         System.out.println("------------------------------------------------------------------------");
     }
 
-
-
-
-
-    //end
 }
 
 
