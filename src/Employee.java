f8b51b17fb1a0626ee2dec1a5d6bd7c90db0ecf2
Mohammad Ras <mohammed.g.aburass@gmail.com>
2025-11-29 21:52:06 +0300

    here is ,I am adding Employee record to represent employee entity

--- src/Employee.java
+++ src/Employee.java
@@ -1,5 +1,5 @@
 import java.time.LocalDate;
-// creating Employee Record:
+// creating Employee Record as follows:
 
 public record Employee (int id,String name, String email, String department, double salary,LocalDate joiningDate) {
 
